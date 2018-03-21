cb7dabf1b05cc9e1c7fda7bc4a1f439ac73f7016
Hulnis <eric.parker13@gmail.com>
2018-03-21 17:42:04 -0400

    added second page for rooms, join input

--- lib/checkers_web/channels/checkers_channel.ex
+++ lib/checkers_web/channels/checkers_channel.ex
@@ -3,14 +3,12 @@ defmodule CheckersWeb.Channel do
 
   alias Checkers.Game
 
-  def join("game:" <> name, player, socket) do
+  def join("game:" <> name, socket) do
     IO.puts("name")
     IO.inspect(name)
-    IO.puts("player")
-    IO.inspect(player)
     IO.puts("socket")
     IO.inspect(socket)
-    
+
     if authorized?(player) do
       game = Checkers.Backup.load(name) || Game.init()
       |> Game.add_player(player)
