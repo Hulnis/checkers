e0507ccfe32f0d50a25d88ba3645232146787c94
Michael Trichilo <michaeltrichilo3@gmail.com>
2018-03-27 20:52:30 -0400

    whose turn

--- lib/checkers_web/channels/checkers_channel.ex
+++ lib/checkers_web/channels/checkers_channel.ex
@@ -16,18 +16,23 @@ defmodule CheckersWeb.Channel do
         |> assign(:player, id)
         Checkers.Backup.save(socket.assigns[:name], game)
         send(self(), :after_join)
-        {:ok, %{"join" => name, "game" => game[:board], "player" => game[:players][id]}, socket}
+        whose_turn = game[:players][game[:current_player]]
+        {:ok, %{"join" => name, "game" => game[:board], 
+          "player" => game[:players][id]} "whoseTurn" => whose_turn, socket}
       end
     end
 
     def handle_info(:after_join, socket) do
-      broadcast_from(socket, "update", %{"game" => socket.assigns[:game]})
+      game = socket.assigns[:game]
+      whose_turn = game[:players][game[:current_player]]
+      broadcast_from(socket, "update", %{"game" => game, "whoseTurn" => whose_turn]})
       {:noreply, socket}
     end
 
     def handle_out("update", %{"game" => game}, socket) do
       socket = assign(socket, :game, game)
-      push(socket, "update", %{"game" => game[:board]})
+      whose_turn = game[:players][game[:current_player]]
+      push(socket, "update", %{"game" => game[:board], "whoseTurn" => whose_turn})
       {:noreply, socket}
     end
 
@@ -36,7 +41,9 @@ defmodule CheckersWeb.Channel do
       socket = assign(socket, :game, game)
       |> assign(:player, id)
       Checkers.Backup.save(socket.assigns[:name], game)
-      push(socket, "restart", %{"game" => game[:board], "player" => game[:players][id]})
+      whose_turn = game[:players][game[:current_player]]
+      push(socket, "restart", %{"game" => game[:board], 
+        "player" => game[:players][id], "whoseTurn" => whose_turn})
       broadcast_from(socket, "update", %{"game" => game, "message" => "Game restarted."})
       {:noreply, socket}
     end
@@ -46,7 +53,8 @@ defmodule CheckersWeb.Channel do
       {message, game} = Game.take_turn(socket.assigns[:game], player, from, to)
       socket = assign(socket, :game, game)
       Checkers.Backup.save(socket.assigns[:name], game)
-      push(socket, "update", %{"game" => game[:board], "message" => message})
+      whose_turn = game[:players][game[:current_player]]
+      push(socket, "update", %{"game" => game[:board], "whoseTurn" => whose_turn, "message" => message})
       broadcast_from(socket, "update", %{"game" => game, "message" => nil})
       if Game.is_winner?(game, player) do
         push(socket, "winner", %{})
@@ -62,7 +70,8 @@ defmodule CheckersWeb.Channel do
       |> assign(:player, id)
       Checkers.Backup.save(socket.assigns[:name], game)
       broadcast_from(socket, "restart", %{"game" => game, "message" => "Game restarted."})
-      {:reply, {:ok, %{"game" => game[:board], "player" => game[:players][id]}}, socket}
+      whose_turn = game[:players][game[:current_player]]
+      {:reply, {:ok, %{"game" => game[:board], "player" => game[:players][id], "whoseTurn" => whose_turn}}, socket}
     end
 
     def handle_in("disconnect", %{}, socket) do
